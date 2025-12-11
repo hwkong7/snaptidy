cb4218e05be67677e26d234680eab2dbf461e4e5
hwkong7 <hwkong7_@naver.com>
2025-12-11 12:44:57 +0900

    미리보기 기능 삭제

--- src/components/PhotoCard/PhotoCard.jsx
+++ src/components/PhotoCard/PhotoCard.jsx
@@ -1,11 +1,25 @@
-// src/components/PhotoCard/PhotoCard.jsx
 import "./PhotoCard.css";
 
-export default function PhotoCard({ image, selected, onClick }) {
+export default function PhotoCard({ image, selected, onClick, onHover }) {
+  let hoverTimer = null;
+
+  const handleMouseEnter = () => {
+    hoverTimer = setTimeout(() => {
+      onHover(image);
+    }, 800); // 0.8초 후 실행
+  };
+
+  const handleMouseLeave = () => {
+    clearTimeout(hoverTimer);
+    onHover(null);
+  };
+
   return (
     <div
       className={`photo-card ${selected ? "selected" : ""}`}
       onClick={onClick}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       {image && <img src={image} alt="thumb" />}
       {selected && <div className="check-icon">✔</div>}
